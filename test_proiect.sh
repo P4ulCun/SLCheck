8e538552f3c4f6eb9d311978743bd4294e6f7431
Paul <yopaulc100@gmail.com>
2024-12-22 16:42:18 +0200

    added the functionality for -follow-symlink flag

--- test_proiect.sh
+++ test_proiect.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# read DIR
 PATH=$1
-# citirea fiecarui element din fisierul dir
+FLAG=$2
+
 function rec_search(){
     DIR=$1
     for file in $DIR/*; do
     if [ -L "$file" ]; then
         echo "${file##*/} is a symlink"
+        if [ "$FLAG" == "-follow-symlink" ]; then
+            echo "follow the symlink: $file"
+            rec_search $file
+        fi
     elif [ -f "$file" ]; then
         echo "${file##*/} is a file"
     elif [ -d "$file" ]; then
