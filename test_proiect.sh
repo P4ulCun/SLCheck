e3bd5dc1b999a19b1851fc38bb6201729ade4081
Paul <yopaulc100@gmail.com>
2024-12-22 20:39:04 +0200

    made it so when -follow-symlink flag is used, the script doesn't go into an infinite loop if the links are set up that way

--- test_proiect.sh
+++ test_proiect.sh
@@ -6,11 +6,16 @@ FLAG=$2
 function rec_search(){
     DIR=$1
     for file in $DIR/*; do
+    # verific pentru fiecare item ce tip de fila e
     if [ -L "$file" ]; then
         echo "${file##*/} is a symlink"
         if [ "$FLAG" == "-follow-symlink" ]; then
-            echo "follow the symlink: $file"
-            rec_search $file
+            # verific mai intai daca am mai trecut prin acest link
+            if [[ "${file%/*}" != *"${file##*/}"* ]]; then
+            # if ul e ca sa nu dea loop infinit
+                echo "follow the symlink: $file"
+                rec_search $file
+            fi
         fi
     elif [ -f "$file" ]; then
         echo "${file##*/} is a file"
